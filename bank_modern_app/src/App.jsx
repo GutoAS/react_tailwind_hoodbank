9c4c3b57dc4cfa21a70785df2a8d8c5b20539a2e
GutoAS <agostinhosoberano2@gmail.com>
2022-12-04 22:52:21 +0200

    Structure & Layout

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -1,7 +1,50 @@
+import styles from "./styles";
 import React from "react";
+import {
+  Billing,
+  Business,
+  Button,
+  CardDeal,
+  Clients,
+  CTA,
+  FeedBackCard,
+  Footer,
+  GetStarted,
+  Hero,
+  Navbar,
+  Stats,
+  Testimonials,
+} from "./components";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
